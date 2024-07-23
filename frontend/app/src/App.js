5099633183739a23af31ced964c39cd000e1416c
Breno <brenomelogabriel2019@gmail.com>
2024-07-23 12:49:03 -0300

    ref

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <h1 className="">Listify</h1>
+        <h2 className="">Keep calm and just do it</h2>
+        <div className="div1">
+            <button className="button1">Listagem de tarefas</button>
+            <button className="button1">Cadastro de Membros</button>
+        </div>
+      </div>
   );
 }
 
